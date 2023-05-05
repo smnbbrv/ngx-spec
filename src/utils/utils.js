b0c7bef2e4caf9092d89d936c9c0f831c1a262f3
Semen Bobrov <smnbbrv@gmail.com>
2023-05-05 09:56:00 +0200

    chore: rebuild

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,21 +3,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getStandardSchematicPath = exports.describeFile = void 0;
 const supported_types_1 = require("./supported-types");
 function describeFile(fullPath) {
-    const [, name, type] = (fullPath.replace(/\.ts$/, '').match(/(.*)\.([^.]+)$/) || [null, null, null]);
+    const [, name, type] = (fullPath
+        .replace(/\.ts$/, "")
+        .match(/(.*)\.([^.]+)$/) || [null, null, null]);
     if (!name || !type) {
         return null;
     }
-    const path = name.replace(/[^/\\]*$/, '');
+    const path = name.replace(/[^/\\]*$/, "");
     return {
-        path: path.replace(/[^/\\]$/, ''),
-        name: name.replace(path, ''),
+        path: path.replace(/[^/\\]$/, ""),
+        name: name.replace(path, ""),
         type,
-        supported: supported_types_1.SupportedTypes.includes(type)
+        supported: supported_types_1.SupportedTypes.includes(type),
     };
 }
 exports.describeFile = describeFile;
 function getStandardSchematicPath(type) {
-    return require.resolve(`@schematics/angular/${type}/index.js`).replace(/index\.js$/, 'files');
+    return require
+        .resolve(`@schematics/angular/${type}/index`)
+        .replace(/index\.js$/, "files");
 }
 exports.getStandardSchematicPath = getStandardSchematicPath;
 //# sourceMappingURL=utils.js.map
